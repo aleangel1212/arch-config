82596b5ff9025d8879a07e6c42cce5ad7b0bbdf2
Alessandro Vecchi <aleangel1212@gmail.com>
2017-01-20 10:25:07 +0000

    removed chromium from .config

--- scripts/install/install-config.sh
+++ scripts/install/install-config.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-sudo mkdir -p /etc/arch_config
-cd ../../config
-
-echo -e "\ncopying .config to /ect/arch_config"
-sudo cp -r .config /etc/arch_config
-
-echo "copying .zshrc to /etc/arch_config"
-sudo cp -r .zshrc /etc/arch_config
-
-echo "copying wallpaper to /etc/arch_config"
-sudo cp -f wallpaper.* /etc/arch_config
-
 echo -e "\ndeleting current ~/.config"
 sudo rm -r ~/.config
 
@@ -19,7 +7,7 @@ echo "deleting current ~/.zshrc"
 sudo rm -r ~/.zshrc
 
 echo -e "\ncreating symlink in home directory for .config"
-ln -s /etc/arch_config/.config ~/.config
+ln -s /etc/arch-config/config/.config ~/.config
 
 echo "creating symlink in home directory for .zshrc"
-ln -s /etc/arch_config/.zshrc ~/.zshrc
+ln -s /etc/arch-config/config/.zshrc ~/.zshrc
